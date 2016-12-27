95dbaf513640b44d2a8b376d3b01924420192e25
Matthew Zimmer <M16r01@gmail.com>
2016-12-27 10:13:39 -0500

    chopped top off a bit more

--- model.py
+++ model.py
@@ -172,7 +172,7 @@ def preprocess_image(image_array, output_shape=(20, 40), colorspace='yuv'):
     # crops top 31.25% portion and bottom 12.5% portion
     #
     # The entire width of the image is preserved
-    image_array = image_array[50:140, 0:320]
+    image_array = image_array[65:140, 0:320]
 
     # Let's blur the image to smooth out some of the artifacts
     kernel_size = 5  # Must be an odd number (3, 5, 7...)
